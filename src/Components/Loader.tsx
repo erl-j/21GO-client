c3165cf9c5e476f55134e2ee2c5edfde1a0e48f2
Zhuowen Fang <zhuowen@kth.se>
2019-03-19 16:14:10 +0100

    fixed loader

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 
 const Loader = () => {
-  return <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>;
+  return (<div className="lds-ellipsis">
+            <div />
+            <div />
+            <div />
+            <div />
+          </div>);
 };
 export default Loader;
