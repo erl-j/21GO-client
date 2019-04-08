f88a58d13f579fd6de9e15c6f14ae0ea97ec3e6c
Zhuowen Fang <zhuowen@kth.se>
2019-04-08 10:00:41 +0200

    Add catalog filter

--- src/Scenes/Catalog/CatalogContainer.tsx
+++ src/Scenes/Catalog/CatalogContainer.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { RouteComponentProps} from 'react-router';
-import {Link} from "react-router-dom"
+// import {Link} from "react-router-dom"
 import * as queryString from 'query-string';
 import SuperorderSummary from '../../Superorder/SuperorderSummary';
 import { connect } from 'react-redux';
 import * as actions from './CatalogActions';
 import Navbar from '../../Components/Navbar';
+import CatalogFilter from './CatalogFilter';
 
 
 interface ICatalogContainerProps {
@@ -36,7 +37,7 @@ class CatalogContainer extends React.Component<RouteComponentProps & ICatalogCon
 	public render() {
 
 		// Test for search. DELETE BEFORE SUBMISSION!
-		
+
 		// const testSearchLink = () => {
 		// 	const param = { sortType: 'createdAt', sortOrder: 'ASC' , tags:["vanessa","stanley"],page:3,dispatch:"PICKUP"};
 		// 	const paramString=queryString.stringify(param);
@@ -44,14 +45,15 @@ class CatalogContainer extends React.Component<RouteComponentProps & ICatalogCon
 		// 	return <Link to={"/catalog/search?"+paramString}>
 
 		// 	<button>test search</button>
-			
+
 		// 	</Link>;
 		// };
 
 
 		return (
-			<div>
+			<div className="catalog">
 				<Navbar isCatalog={true}/>
+				<CatalogFilter />
 				<h3>This is a catalog search with parameters</h3>
 				<div className="row">
 					{this.props.searchResults.map(res => (
---
+++ src/Scenes/Catalog/Tag.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import deleteIcon from '../../img/icons/cross_white.svg';
+
+const Tag = ({tag, index, onDeleteClick}) => {
+  const handleClick = () => {
+    onDeleteClick(index);
+  }
+
+  return (
+    <span>
+      {tag}
+      <img className="delete" src={deleteIcon} alt="delete" onClick={handleClick}/>
+    </span>
+  );
+};
+
+export default Tag;
