338429720a51089c73ec1c766f9a3f17832e6ab5
ssssarah <mouffoksarah1@gmail.com>
2019-04-18 13:59:24 +0200

    lower case o for superorder somewhere

--- src/rootReducer.tsx
+++ src/rootReducer.tsx
@@ -5,13 +5,16 @@ import catalogReducer from "./Scenes/Catalog/CatalogReducer";
 import setSuperorderReducer from "./Scenes/SetSuperorder/SetSuperorderReducer";
 import setOrderReducer from "./Scenes/SetOrder/SetOrderReducer";
 import accountReducer from "./Scenes/UserAccount/UserProfile/UserProfileReducer";
+import userSuperordersReducer from "./Scenes/UserAccount/UserSuperorders/UserSuperordersReducer";
+
 const rootReducer = combineReducers({
     signIn:signInReducer,
     signUp:signUpReducer,
     catalog:catalogReducer,
     setSuperorder:setSuperorderReducer,
     setOrder:setOrderReducer,
-    account:accountReducer
+    account:accountReducer,
+    userSuperorders: userSuperordersReducer
 });
 
 export default rootReducer;
