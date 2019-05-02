35f61e6fb496680e79dfdbd5ab180203620a6fb2
Zhuowen Fang <zhuowen@kth.se>
2019-05-02 17:57:14 +0200

    Details view for orders and some responsiveness

--- src/Scenes/UserAccount/UserOrders/UserOrder.tsx
+++ src/Scenes/UserAccount/UserOrders/UserOrder.tsx
@@ -1,41 +1,41 @@
 import * as React from "react";
-
 const UserOrder = (props) => {
 
     const superorder = props.superorder;
     const order = superorder.myOrder;
+    let itemAmount = 0;
 
-    const items = order.orderItems.map((orderItem) =>
-        <div key = {orderItem.id}>
-            <h3>ID {orderItem.id}</h3>
-            <h3>Additional Info {orderItem.additionalInfo}</h3>
-            <h3>Quantity {orderItem.quantity}</h3>
-        </div>
-    );
+    const items = order.orderItems.map((orderItem) => {
+      itemAmount += orderItem.quantity;
+      // return (
+      //   <div key = {orderItem.id}>
+      //       <h3>ID {orderItem.id}</h3>
+      //       <h3>Additional Info {orderItem.additionalInfo}</h3>
+      //       <h3>Quantity {orderItem.quantity}</h3>
+      //   </div>
+      // );
+      return '';
+    });
 
     return (
         <React.Fragment>
-            <h3>ID {superorder.id}</h3>
-            <h3>Store URL {superorder.storeURL}</h3>
-            <h3>Store Location {superorder.storeLocation}</h3>
-            <h3>Deadline {superorder.deadline}</h3>
-            <h3>Arrival Location {superorder.arrivalLocation}</h3>
-            <h3>Available Dispatch {superorder.availableDispatch}</h3>
-            <h3>Store Name {superorder.storeName}</h3>
-            <h3>Tags {superorder.tags}</h3>
-            <h3>Created At {superorder.createdAt}</h3>
-            <h3>Is Deleted {superorder.isDeleted}</h3>
-
-            <h2>My Order</h2>
-
-            <React.Fragment>
-                <h3>ID {order.id}</h3>
-                <h3>Dispatch {order.dispatch}</h3>
-                <h3>Status {order.status}</h3>
-                <h3>Is Deleted {order.isDeleted}</h3>
-                {items}
-            </React.Fragment>
-
+					<div className="account-items semi-bold" onClick={props.seeDetails}>
+						<div className="box1">
+							<img className="item-img" src={superorder.imageUrl} alt="Superorder image" />
+						</div>
+						<div className="box2">
+							<span>{superorder.storeName + ", " + superorder.storeLocation}</span>
+							<span>#{order.id}</span>
+							<span>From {superorder.user.firstName + " " + superorder.user.lastName}</span>
+						</div>
+						<div className="box3">
+							<span>{itemAmount} {itemAmount > 1 ? 'Items' : 'Item'}</span>
+							<span>{order.dispatch}</span>
+							<span>{order.status}</span>
+              {order.isDeleted ? <span className="error">Deleted by me</span> : superorder.isDeleted ? <span className="error">Deleted by initiator</span> : ''}
+						</div>
+					</div>
+          {items}
         </React.Fragment>
     );
 
